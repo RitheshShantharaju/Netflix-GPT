1f89faffef087b72d7e17f1c219f5ca6ed8338ed
Rithesh s <ritheshmallaiah@gmail.com>
2025-09-11 20:33:48 +0530

    initial comit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="card">
+        <h1>Namste Everyone!!! lets build react</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
